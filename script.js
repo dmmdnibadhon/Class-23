a9325192e56fbc48161347be5363effc8ad294e8
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-03-29 22:50:58 -0700

    Class-23

---
+++ script.js
@@ -0,0 +1,52 @@
+// Object data type
+let me = {name:"Badhon", firstname:"Nazmul", age:24};
+// console.log(me.firstname);
+console.log(me);
+
+// Array data type
+let my = ["MD. Nazmul", "Islam", "Siddique", 24, "Khan" ];
+console.log(my);
+
+// Function
+function addition(){
+    let x = 10;
+    let y = 22;
+     console.log(x + y);
+}
+addition()
+// addition()
+// addition()
+
+function substraction(){
+    let x = 22;
+    let y = 10;
+
+    console.log(x - y);
+}
+substraction()
+// substraction()
+// substraction()
+
+// Condition
+let age = 30;
+if(age > 18){
+    console.log("Can not job");
+}else if(age > 25){
+    console.log("Can job");
+}
+
+
+// Loop
+
+// While loop
+let x = 0;
+while(x < 10){
+    console.log(x);
+    x++
+}
+
+
+// For loop
+for(let y = 0; y < 10; y++){
+    console.log(y);
+}
